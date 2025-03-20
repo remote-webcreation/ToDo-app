60089fc80607a7c0772d76d3086c07679921a57c
Mandy Jendroska <soulfreelancer@yahoo.com>
2025-03-20 17:39:34 +0100

    changed date format

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 
-document.getElementById("heute").textContent = new Date().toISOString().split("T")[0];
+function formatDate(date) {
+    const day = String(date.getDate()).padStart(2, '0'); // Tag mit führender Null
+    const month = String(date.getMonth() + 1).padStart(2, '0'); // Monat mit führender Null (Monate sind 0-indexiert)
+    const year = date.getFullYear(); // Jahr
+    return `${day}-${month}-${year}`; // Formatierung des Datums
+}
+// Heute-Datum in das gewünschte Format setzen
+document.getElementById("heute").textContent = formatDate(new Date());
